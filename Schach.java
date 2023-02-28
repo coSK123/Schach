48e0453cf11ef86b628d9d71e38cc327d691a5c7
coSK123 <skollmer03@gmail.com>
2023-02-28 20:50:18 +0100

    Added setup for the board and a visualization

--- Schach.java
+++ Schach.java
@@ -1,23 +1,59 @@
 import packagePieces.*;
+import java.util.*;
 public class Schach{
-    static String [] [] Board;
-
+    static Piece [] [] Board = new Piece[8][8];
+ 
     public static void Initialize(){
-        Board [0] [0] = "Rook";
-        Board [0] [1] = "Knight";
-        Board [0] [2] = "Bishop";
-        Board [0] [3] = "Queen";
-        Board [0] [4] = "King";
-        Board [0] [5] = "Bishop";
-        Board [0] [6] = "Knight";
-        Board [0] [7] = "Rook";
+        for(int i = 0; i<8; i++)
+        {
+            for(int b = 0; b<8;b++)
+            {
+                if(i==1 || i==6){
+                    Board[i][b] = new Piece("Pawn", true,0,b,i);
+                }
+                else if(i ==0 || i==7)
+                {
+                    Board[i] [0]= new Piece("Rook", true,0,b,i);
+                    Board[i] [1]= new Piece("Knight", true,0,b,i);
+                    Board[i] [2]= new Piece("Bishop", true,0,b,i);
+                    Board[i] [3]= new Piece("King", true,0,b,i);
+                    Board[i] [4]= new Piece("Queen", true,0,b,i);
+                    Board[i] [5]= new Piece("Bishop", true,0,b,i);
+                    Board[i] [6]= new Piece("Knight", true,0,b,i);
+                    Board[i] [7]= new Piece("Rook", true,0,b,i);
+                }
+                else
+                {
+                    Board[i][b] = new Piece("x", true,0,b,i);
+                }
+            }
+        }
     }
-  
+public static void printBoard()
+{
+    for(int i = 0; i<8; i++)
+    {   
+        System.out.print("|");
+        for(int b = 0; b<8;b++)
+        {
+            if(Board[i][b].name != "x"){
+                System.out.print(Board[i][b].name);
+                for(int f = 8- Board[i][b].name.length();f>0;f-- ){
+                    System.out.print(" ");
+                }
+                System.out.print("|");
+            }
+            else{
+              System.out.print("xxxxxxxx|");
+            }
+            
+        }
+        System.out.println("");
+    }
+}
 
 public static void main(String[] args){
     Initialize();
-    System.out.println(Board[0][0]);
-    Piece King = new Piece("King", true, 1,0,0);
-    System.out.println(King.name);
+    printBoard();
 }
 }
